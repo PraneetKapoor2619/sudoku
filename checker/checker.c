8bca9559df097db905005fbbd9b9d541a596e5bc
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2021-12-18 11:22:12 +0530

    Complete Sudoku checker. Checks rows, columns, and blocks. Input can be taken from stdin or from a file

--- checker/checker.c
+++ checker/checker.c
@@ -2,96 +2,127 @@
 #include<stdlib.h>
 #include<stdint.h>
 
-void enterrow(uint8_t *);
-void rowcheck(uint8_t *);
+void entermatrix();
+void printmatrix();
+void resetflag();
+void rowcheck(uint8_t);
 void columncheck(uint8_t);
+void blockcheck(uint8_t);
+void check();
 
 struct grid
 {
-	uint8_t row1[9];
-	uint8_t row2[9];
-	uint8_t row3[9];
-	uint8_t row4[9];
-	uint8_t row5[9];
-	uint8_t row6[9];
-	uint8_t row7[9];
-	uint8_t row8[9];
-	uint8_t row9[9];
+	uint8_t matrix[9][9];
 	uint8_t f[9];
 };
-struct grid grid1;
+struct grid g1;
 
 int main(int argc, char ** argv)
 {
-	enterrow(grid1.row1);
-	enterrow(grid1.row2);
-	enterrow(grid1.row3);	
-	enterrow(grid1.row4);
-	enterrow(grid1.row5);
-	enterrow(grid1.row6);
-	enterrow(grid1.row7);
-	enterrow(grid1.row8);
-	enterrow(grid1.row9);
-	rowcheck(grid1.row1);
-	rowcheck(grid1.row2);
-	rowcheck(grid1.row3);
-	rowcheck(grid1.row4);
-	rowcheck(grid1.row5);
-	rowcheck(grid1.row6);
-	rowcheck(grid1.row7);
-	rowcheck(grid1.row8);
-	rowcheck(grid1.row9);
-	columncheck(1);
-	columncheck(2);
-	columncheck(3);
-	columncheck(4);
-	columncheck(5);
-	columncheck(6);
-	columncheck(7);
-	columncheck(8);
-	columncheck(9);
-	int flag = 0;
+	entermatrix();
+	printmatrix();
+	resetflag();
 	for(int i = 0; i < 9; ++i)
 	{
-		if(grid1.f[i] != 18)
-		{
-			printf("ERROR in %d\n", i);
-			++flag;
-		}
+		rowcheck(i);
+		columncheck(i);
+		blockcheck(i + 1);
+
 	}
-	if(flag > 0)
-	{
-		exit(0);
-	}
-	else
-	{
-		printf("ALL CLEAR\n");
-	}
+	check();
 	return 0;
 }
 
-void enterrow(uint8_t *ptr)
+void entermatrix()
 {
-	scanf("%d %d %d %d %d %d %d %d %d", ptr, ptr + 1, ptr + 2, ptr + 3, ptr + 4, ptr + 5, ptr + 6, ptr + 7, ptr + 8);
-}
-
-void rowcheck(uint8_t *ptr)
-{
+	FILE *fp;
+	fp = fopen("grid.txt", "r");
+	if(fp == NULL)
+	{
+		fprintf(stdout, "\nNo file detected!!\n");
+		exit(0);
+	}
 	for(int i = 0; i < 9; ++i)
 	{
-		++grid1.f[*(ptr + i) - 1];	
+		fscanf(fp, "%d %d %d %d %d %d %d %d %d", 
+			&g1.matrix[i][0],
+			&g1.matrix[i][1],
+			&g1.matrix[i][2],
+			&g1.matrix[i][3],
+			&g1.matrix[i][4],
+			&g1.matrix[i][5],
+			&g1.matrix[i][6],
+			&g1.matrix[i][7],
+			&g1.matrix[i][8]);
 	}
 }
 
-void columncheck(uint8_t cno)
+void printmatrix()
 {
-	++grid1.f[grid1.row1[cno - 1] - 1];
-	++grid1.f[grid1.row2[cno - 1] - 1];
-	++grid1.f[grid1.row3[cno - 1] - 1];
-	++grid1.f[grid1.row4[cno - 1] - 1];
-	++grid1.f[grid1.row5[cno - 1] - 1];
-	++grid1.f[grid1.row6[cno - 1] - 1];
-	++grid1.f[grid1.row7[cno - 1] - 1];
-	++grid1.f[grid1.row8[cno - 1] - 1];
-	++grid1.f[grid1.row9[cno - 1] - 1];
+	for(int i = 0; i < 9; ++i)
+	{
+		printf("\n");
+		for(int j = 0; j < 9; ++j)
+		{
+			printf("%d ", g1.matrix[i][j]);
+		}
+	}
+	printf("\n");
+}
+
+void resetflag()
+{
+	for(int i = 0; i < 9; ++i)
+	{
+		g1.f[i] = 0;
+	}
+}
+
+void rowcheck(uint8_t R)
+{
+	for(int i = 0; i < 9; ++i)
+	{
+		++g1.f[g1.matrix[R][i] - 1];
+	}
+}
+
+void columncheck(uint8_t C)
+{
+	for(int i = 0; i < 9; ++i)
+	{
+		++g1.f[g1.matrix[i][C] - 1];
+	}
+}
+
+void blockcheck(uint8_t B)
+{
+	int R, C;
+	R = (int)((B - 1) / 3);
+	R *= 3;
+	C = (int)((B - 1) % 3);
+	C *= 3;
+	for(int i = R; i < R + 3; ++i)
+	{
+		for(int j = C; j < C + 3; ++j)
+		{
+			++g1.f[g1.matrix[i][j] - 1];
+		}
+	}
+}
+
+void check()
+{
+	int val = 0;
+	for(int i = 0; i < 9; ++i)
+	{
+		if(g1.f[i] != 27)
+		{
+			printf("Error in %d\n", i + 1); 
+			++val;
+		}
+	}
+	if(val == 0)
+		printf("The solution is correct\n");
+	else
+		printf("Incorrect solution\n");
 }
